21c4024b2c1efdb2ee7fff908c8570fed8f87cd5
Inna Kozhina <inna.kozhina22@gmail.com>
2023-12-19 15:06:28 +0200

    search through authors and title added

--- src/routes/Books.jsx
+++ src/routes/Books.jsx
@@ -1,5 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useAxios from "../services/useAxios";
+import TextField from "@mui/material/TextField";
+
 import {
   Box,
   Card,
@@ -15,12 +17,15 @@ import {
 
 //render all books together
 function Books() {
+  const [search, setSearch] = useState([]);
+
   const { data, loading, get } = useAxios("http://localhost:3000");
   useEffect(() => {
     if (data.length === 0) {
       getBooks();
     }
   }, []);
+  console.log(data);
 
   // TODO: Replace axios with useAxios hook
   function getBooks() {
@@ -28,70 +33,91 @@ function Books() {
   }
 
   // TODO: Implement search functionality
+  function handleSearch(e) {
+    setSearch(e.target.value.toLowerCase());
+  }
+  console.log("search", search);
+
+  const filteredArray = data.filter(
+    (book) =>
+      book.name.toLowerCase().includes(search) ||
+      book.author.toLowerCase().includes(search)
+  );
   return (
-    <Box sx={{ mx: "auto", p: 2 }}>
-      {loading && <CircularProgress />}
-      {!loading && (
-        <div>
-          <Stack
-            sx={{ justifyContent: "space-around" }}
-            spacing={{ xs: 1 }}
-            direction="row"
-            useFlexGap
-            flexWrap="wrap"
-          >
-            {data.map((book) => (
-              <Card
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  width: "15%",
-                  minWidth: 200,
-                }}
-                key={book.name}
-              >
-                <CardMedia
-                  sx={{ height: 250 }}
-                  image={book.img}
-                  title={book.name}
-                />
-                <Box sx={{ pt: 2, pl: 2 }}>
-                  {book.genres.map((genre, i) => (
-                    <Chip
-                      key={i}
-                      label={genre}
-                      variant="outlined"
+    <>
+      <TextField
+        name="search"
+        id="outlined-basic"
+        label="search"
+        variant="outlined"
+        onChange={handleSearch}
+        value={search}
+      />
+
+      <Box sx={{ mx: "auto", p: 2 }}>
+        {loading && <CircularProgress />}
+        {!loading && (
+          <div>
+            <Stack
+              sx={{ justifyContent: "space-around" }}
+              spacing={{ xs: 1 }}
+              direction="row"
+              useFlexGap
+              flexWrap="wrap"
+            >
+              {filteredArray.map((book) => (
+                <Card
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    width: "15%",
+                    minWidth: 200,
+                  }}
+                  key={book.name}
+                >
+                  <CardMedia
+                    sx={{ height: 250 }}
+                    image={book.img}
+                    title={book.name}
+                  />
+                  <Box sx={{ pt: 2, pl: 2 }}>
+                    {book.genres.map((genre, i) => (
+                      <Chip
+                        key={i}
+                        label={genre}
+                        variant="outlined"
+                        size="small"
+                      />
+                    ))}
+                    <Typography variant="h6" component="h2" sx={{ mt: 2 }}>
+                      {book.name}
+                    </Typography>
+                    <Typography variant="subtitle1" gutterBottom>
+                      {book.author}
+                    </Typography>
+                  </Box>
+                  <CardActions
+                    sx={{
+                      justifyContent: "space-between",
+                      mt: "auto",
+                      pl: 2,
+                    }}
+                  >
+                    <Rating
+                      name="read-only"
+                      value={book.stars}
+                      readOnly
                       size="small"
                     />
-                  ))}
-                  <Typography variant="h6" component="h2" sx={{ mt: 2 }}>
-                    {book.name}
-                  </Typography>
-                  <Typography variant="subtitle1" gutterBottom>
-                    {book.author}
-                  </Typography>
-                </Box>
-                <CardActions
-                  sx={{
-                    justifyContent: "space-between",
-                    mt: "auto",
-                    pl: 2,
-                  }}
-                >
-                  <Rating
-                    name="read-only"
-                    value={book.stars}
-                    readOnly
-                    size="small"
-                  />
-                  <Button size="small">Learn More</Button>
-                </CardActions>
-              </Card>
-            ))}
-          </Stack>
-        </div>
-      )}
-    </Box>
+                    <Button size="small">Learn More</Button>
+                  </CardActions>
+                </Card>
+              ))}
+            </Stack>
+          </div>
+        )}
+      </Box>
+    </>
   );
 }
 
